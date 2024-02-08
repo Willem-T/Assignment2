6b3970f4a3805ec750505232273b0f7f8a341ad6
Willem Toews <willem.toews@lethbridgecollege.ca>
2024-02-08 11:34:03 -0700

    started logic

--- app/game/gameMain.js
+++ app/game/gameMain.js
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from '../styles/gameStyle.js';
-import {Text, View, Button,} from 'react-native';
+import {Text, View, Button, Pressable} from 'react-native';
 import { Link,} from 'expo-router';
 
+
 export default function Page(){
-    let score = 1300; //stub
+    const [score, setScore] = useState(0);
+    const [life, setLife ] = useState(3);
+    const [ mole, setMole ] = useState(false);
+    const [timerActive, setTimerActive] = useState(false);
+
+        // Run the random timer when the component mounts
+        useEffect(() => {
+            runRandomTimer();
+        }, []);
+
+    //handle the onPress event
+    const handlePress = () => {
+        //mole is there
+        if(mole){
+            setScore(f => f +1);
+            setMole(false);
+        }
+        //bros gone
+        else{
+            //needs some logic for 0
+            setLife(f => f -1);
+        }
+    }
+
+    function runRandomTimer() {
+        //MAKES SURE TO NOT RUN MULTIPLE TIMERS
+        if (timerActive) return;
+
+        //mole delay for time before he appears
+        const moleDelay = Math.floor(Math.random() * 5000) + 1000;
+
+        //speed
+        const speed = 4000; //needs a multiplier
+
+        setTimerActive(true);
+
+        setTimeout(() => {
+            setMole(true);
+            //timeout to make the mole disapear
+            setTimeout(() => {
+                setTimerActive(false); //enable timers
+
+                //JS sucks and this is fucking broken
+                //has something todo with callbacks not being updated
+                // if (!mole) { //might change this later if its too hard
+                //     setLife(f => f -1);
+                // }
+                setMole(false); 
+                runRandomTimer(); 
+            }, speed); //set this to a speed var
+        }, moleDelay);
+    }
 
     return (
         
-        <View style={Styles.game}>
+        <View style={Styles.container}>
             {/* Score */}
             <Text style={Styles.score}>Score: {score}</Text>
-            {/* Level?  */}
-
-            {/* init starting params for speed, update with score */}
-
-            
+            <Text style={Styles.score}>Life: {life}</Text>
+ 
+            <View style={Styles.buttonRow}>
+            <Pressable style={mole === true ? Styles.mole : Styles.buttonBox} onPress={() => handlePress()}/>
+            </View>
             {/* Linked Buttons */}
             <View style={Styles.backButton} >
             <Link href={{
@@ -28,19 +80,8 @@ export default function Page(){
             </View>
 
         </View>
+        //pass score to gameEnd.js
+        // Stop?
     )
 }
-
-// double for loop to create grid
-
-    // for height
-    //create pressable
-    // pass init speed param and update it according to speed
-
-        //for width
-        //create pressable
-
-
-//pass score to gameEnd.js
-// Stop?
 
--- app/styles/gameStyle.js
+++ app/styles/gameStyle.js
@@ -2,17 +2,40 @@ import { StyleSheet } from "react-native";
 
 
 const styles = StyleSheet.create({
-    game: {
+    container: {
         flex: 1,
-        backgroundColor: '#ffffff',
-    },
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
     score: {
         fontSize: 35,
         textAlign: 'center',
     },
     backButton: {
         width: '30%',
-    }
+    },
+    buttonRow: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      buttonBox: {
+        height: 70,
+        width: 70,
+        borderRadius: 5,
+        margin: 3,
+        padding: 10,
+        backgroundColor: 'black',
+      },
+      mole: {
+        height: 70,
+        width: 70,
+        borderRadius: 5,
+        margin: 3,
+        padding: 10,
+        backgroundColor: 'brown',
+      }
 });
 
 export default styles;
